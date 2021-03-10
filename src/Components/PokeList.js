236d14a34593ff6de9fff80a3e566be386a36dbe
CCrisBIB <cristinabiglesias@gmail.com>
2021-03-10 20:01:04 +0100

    Add onClick event to check as favorite

--- src/Components/PokeList.js
+++ src/Components/PokeList.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "../Styles/App.scss";
 import PokemonCard from "./PokemonCard";
+import "../Styles/PokeList.scss";
+import PropTypes from "prop-types";
 
 class PokeList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render(props) {
+  render() {
     const eachPokemon = this.props.pokemonData.map((pokemonItem) => {
       return (
         <li key={pokemonItem.id} className="eachPokemon">
@@ -18,3 +17,7 @@ class PokeList extends React.Component {
   }
 }
 export default PokeList;
+
+PokeList.propTypes = {
+  pokemonData: PropTypes.array,
+};
--- src/Components/PokemonCard.js
+++ src/Components/PokemonCard.js
@@ -1,18 +1,43 @@
 import "../Styles/App.scss";
+import "../Styles/PokemonCard.scss";
+import PropTypes from "prop-types";
+import { useState } from "react";
 
 const PokemonCard = (props) => {
+  const [favorite, setFavorite] = useState("");
+
+  const checkFavorite = () => {
+    if (favorite === "") {
+      setFavorite("favPokemon");
+    } else {
+      return setFavorite("");
+    }
+  };
+
   const types = props.infoPokemon.types.map((type, i) => (
     <li key={i} className="eachType">
       {type}
     </li>
   ));
+  const handleClick = () => {
+    checkFavorite();
+  };
   return (
-    <>
-      <img src={props.infoPokemon.url} alt="" />
-      <h3 className="pokemonName">{props.infoPokemon.name}</h3>
+    <article onClick={handleClick}>
+      <img
+        src={props.infoPokemon.url}
+        alt={"Imageden de: " + props.infoPokemon.name}
+      />
+      <h3 className={`pokemonName ${favorite}`}>{props.infoPokemon.name}</h3>
       <ul className="pokemonTypes">{types}</ul>
-    </>
+    </article>
   );
 };
 
 export default PokemonCard;
+
+PokemonCard.propTypes = {
+  types: PropTypes.array,
+  name: PropTypes.string,
+  url: PropTypes.string,
+};
